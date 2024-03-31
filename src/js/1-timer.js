8da5d1346560ac66546a326581d8700e9d48c8cb
george9907 <109024372+george9907@users.noreply.github.com>
2024-03-31 15:22:11 +0200

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -17,7 +17,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
     userDate = selectedDates[0];
     timeInterval = userDate - options.defaultDate;
 
